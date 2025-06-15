f039d1021dc2b6268ce11489abad506f64724008
thomas eduardo <thomaseduardoss@hotmail.com>
2025-06-15 11:49:44 -0300

    Microondas finalizado.

--- MicroOndas.Core/Microondas.cs
+++ MicroOndas.Core/Microondas.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -7,152 +6,69 @@ namespace MicroOndas.Core
 {
     public class Microondas
     {
-        private int _tempoSegundos;
+        private int _tempo;
         private int _potencia;
-        private char _caractere;
         private CancellationTokenSource _cts;
-        private bool _pausado;
-        private bool _emAquecimento;
 
-        public event Action<string> OnAtualizarAquecimento;
-        public event Action<string> OnFinalizarAquecimento;
-
-        public bool EmAquecimento => _emAquecimento;
-        public bool Pausado => _pausado;
-
-        public Microondas()
+        public void DefinirTempo(int tempo)
         {
-            Resetar();
+            _tempo = tempo;
         }
 
-        public void Iniciar(int? tempo = null, int? potencia = null, char caractere = '.')
+        public void DefinirPotencia(int potencia)
         {
+            _potencia = potencia;
+        }
 
-            if (!tempo.HasValue && !potencia.HasValue)
-            {
-                tempo = 30;
-                potencia = 10;
-            }
-
-            if (!tempo.HasValue)
-                throw new ArgumentException("O tempo precisa ser informado.");
-
-            if (tempo < 1 || tempo > 120)
-                throw new ArgumentException("O tempo tem que ser entre 1 segundo e 2 Minutos.");
-
-            if (potencia.HasValue && (potencia < 1 || potencia > 10))
-                throw new ArgumentException("a potencia tem que estar entre 1 e 10");
-
-
-            _tempoSegundos = tempo.Value;
-            _potencia = potencia ?? 10;
-            _caractere = caractere;
-            _pausado = false;
-
-            if (_emAquecimento && !_pausado)
-            {
-                _tempoSegundos = Math.Min(_tempoSegundos + 30, 120);
-                return;
-            }
-
-            _emAquecimento = true;
+        public void IniciarAquecimento(Action<string> atualizarVisor)
+        {
             _cts = new CancellationTokenSource();
+            var token = _cts.Token;
 
-            Task.Run(() => Aquecer(_cts.Token));
-        }
-        public string IniciarAquecimento(int tempo, int potencia, CancellationToken token)
-        {
-            ValidarParametros(tempo, potencia);
-
-            var resultado = new StringBuilder();
-
-            for (int i = 0; i < tempo; i++)
+            Task.Run(() =>
             {
-                token.ThrowIfCancellationRequested();
+                int segundosRestantes = _tempo;
 
-                resultado.Append(new string('.', potencia));
-                Thread.Sleep(1000); 
-            }
-
-            resultado.Append(" Aquecimento concluído!");
-            return resultado.ToString();
-        }
-        public string InicioRapido(CancellationToken token)
-        {
-            int tempoPadrao = 30;
-            int potenciaPadrao = 8;
-
-            return IniciarAquecimento(tempoPadrao, potenciaPadrao, token);
-        }
-
-        private void ValidarParametros(int tempo, int potencia)
-        {
-            if (tempo < 1 || tempo > 120)
-                throw new ArgumentException("Tempo deve estar entre 1 e 120 segundos.");
-
-            if (potencia < 1 || potencia > 10)
-                throw new ArgumentException("Potência deve estar entre 1 e 10.");
-        }
-        private async Task Aquecer(CancellationToken token)
-        {
-            var sb = new StringBuilder();
-
-            for (int i = 0; i < _tempoSegundos; i++)
-            {
-                if (token.IsCancellationRequested || _pausado)
-                    return;
-
-                for (int j = 0; j < _potencia; j++)
+                while (segundosRestantes > 0 && !token.IsCancellationRequested)
                 {
-                    sb.Append(_caractere);
+                    string tempoFormatado = TimeSpan.FromSeconds(segundosRestantes).ToString(@"mm\:ss");
+
+                    atualizarVisor?.Invoke(tempoFormatado);
+                    Thread.Sleep(1000);
+                    segundosRestantes--;
                 }
 
-                OnAtualizarAquecimento?.Invoke(sb.ToString());
-
-                await Task.Delay(1000, token);
-            }
-
-            _emAquecimento = false;
-            OnFinalizarAquecimento?.Invoke(sb.ToString() + "\nAquecimento concluído.");
+                if (!token.IsCancellationRequested)
+                {
+                    atualizarVisor?.Invoke("Aquecido!");
+                    Console.Beep(); 
+                }
+            }, token);
         }
 
-        public void PausarOuCancelar()
+        public void CancelarAquecimento()
         {
-            if (_emAquecimento && !_pausado)
-            {
-                _pausado = true;
-                _cts.Cancel();
-            }
-            else if (_pausado)
-            {
-                Resetar();
-            }
-            else
-            {
-                Resetar();
-            }
-        }
-
-        public void Retomar()
-        {
-            if (_pausado)
-            {
-                _pausado = false;
-                _cts = new CancellationTokenSource();
-                Task.Run(() => Aquecer(_cts.Token));
-            }
-        }
-
-        public void Resetar()
-        {
-            _tempoSegundos = 0;
-            _potencia = 10;
-            _caractere = '.';
-            _emAquecimento = false;
-            _pausado = false;
-
             _cts?.Cancel();
-
         }
-  }
+
+        public void SelecionarProgramaRapido(string nomePrograma, Action<string> atualizarVisor)
+        {
+            switch (nomePrograma)
+            {
+                case "Pipoca":
+                    DefinirTempo(30);
+                    DefinirPotencia(8);
+                    break;
+                case "Refeição":
+                    DefinirTempo(60);
+                    DefinirPotencia(10);
+                    break;
+                default:
+                    atualizarVisor?.Invoke("Programa não encontrado");
+                    return;
+            }
+
+            IniciarAquecimento(atualizarVisor);
+        }
+    }
 }
